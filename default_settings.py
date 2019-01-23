46a04d9a9a08c0b5b8a3b868dd4f7b4761f5e589
Satoshi Kawasaki <hobbes3@gmail.com>
2019-01-23 00:54:56 +0000

    Better comments.

--- default_settings.py
+++ default_settings.py
@@ -5,20 +5,25 @@ HEC_TOKEN = "b71b8557-2286-4222-97d2-2940c003a763"
 
 THREADS = 24
 
+# How many seconds a thread sleeps after a successful HEC send.
 SLEEP = 0.1
+# How many seconds a thread waits for a response
+# before declaring a timeout (and adding toward the ERROR_LIMIT).
 TIMEOUT = 3
-# How many seconds to sleep before trying to send the data via HEC again.
-# If the last number is reached and the check still fails,
-# then the whole script will exit and the current file list will be saved as
-# a csv at SAVED_FILE_LIST_PATH below.
-# This is to prevent the script from hanging while Splunk is down.
+# How many seconds a thread sleeps before trying to send the file via HEC again.
+# The thread will sleep for each number in order until the last number is reached.
+# Then it will repeat on the last number forever until ERROR_LIMIT is reached.
+# The thread resets back to the first sleep number after a successful send.
 TRY_SLEEP = [0.5, 1, 2, 3, 5, 10, 15, 30]
+# How many total errors (timeouts, HTTP error codes, etc.) allowed among all threads
+# before the entire script exits (and saves the remaining file list).
+# This is to prevent the script from hanging while Splunk is down.
 ERROR_LIMIT = 100
 
 SAVED_FILE_LIST_PATH = "/mnt/data/samples/mass_index_saved_file_list.csv"
 
-# Prefix to add to Splunk's source field.
-# The source is set to this prefix and the filename.
+# The script overrides Splunk's default source for HEC with this prefix and the filename.
+# Change this to an empty string "" if you don't want the prefix.
 SOURCE_PREFIX = "hec::"
 
 LOG_PATH = "/mnt/data/samples/mass_index.log"
